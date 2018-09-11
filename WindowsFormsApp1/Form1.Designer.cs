2fb07e4d3245dc20e4d7fb70e560e53276ad4d6c
MIANGEL PATA <MIAPATA@uat.edu>
2018-09-11 15:41:39 -0700

    It can now fully work. Create items in listboxes and remove them. You can even play the sounds. I also added a sleep option. (:

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -35,6 +35,10 @@
             this.label2 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.sleepText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -98,11 +102,52 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(345, 385);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(168, 99);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Add";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // sleepText
+            // 
+            this.sleepText.Location = new System.Drawing.Point(48, 101);
+            this.sleepText.Name = "sleepText";
+            this.sleepText.Size = new System.Drawing.Size(100, 20);
+            this.sleepText.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Sleep Time";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(496, 414);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sleepText);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label2);
@@ -127,6 +172,10 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox sleepText;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -8,11 +8,13 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Threading;
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+
+        public StringBuilder stringBuilder;
         public Form1()
         {
             InitializeComponent();
@@ -26,30 +28,57 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string beep_item = "Beep: Hertz= " + hertzText.Text + " Interval - " + intervalText.Text;
+            string beep_item = "Hertz=" + hertzText.Text + " Interval=" + intervalText.Text;
             listBox1.Items.Add(beep_item);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            int hertz;
-            int interval;
 
-            foreach(var item  in listBox1.Items)
+            string interval_String;
+            string hertz_String;
+
+            foreach (var item  in listBox1.Items)
             {
-                foreach  (char ch in item.ToString())
+                if (item.ToString().Contains('='))
                 {
-                    if (Char.IsNumber(ch))
-                    {
-                            
-                    }
+                    //This can give us the interval
+                    interval_String = item.ToString().Split('=').Last();
+
+                    //This can give us the hertz
+                    hertz_String = item.ToString().Split('=', 'I')[1].TrimEnd(' ');
+
+
+                    Console.Beep(Convert.ToInt32(hertz_String), Convert.ToInt32(interval_String));
+                }
+                else
+                {
+                    Thread.Sleep(Convert.ToInt32( item.ToString().Split(':').Last())); 
                 }
             }
+
+            
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string item = "Sleep:" + sleepText.Text;
+            listBox1.Items.Add(item);
         }
     }
 }
